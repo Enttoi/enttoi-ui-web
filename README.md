ea461ebef1a0547a87540f0b528d4ed50be672de
Jenya Y <jenyay@gmail.com>
2015-10-04 14:13:22 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,6 @@
+# Enttoi Web Client 
+
+*This repository is part of [Enttoi](http://enttoi.github.io/) project.*
+
+[Aurelia](https://github.com/aurelia) based dashboard.
+
---
+++ build/tasks/prepare-release.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var paths = require('../paths');
+var fs = require('fs');
+var bump = require('gulp-bump');
+var args = require('../args');
+
+// utilizes the bump plugin to bump the
+// semver for the repo
+gulp.task('bump-version', function(){
+  return gulp.src(['./package.json'])
+    .pipe(bump({type:args.bump })) //major|minor|patch|prerelease
+    .pipe(gulp.dest('./'));
+});
+
+// calls the listed sequence of tasks in order
+gulp.task('prepare-release', function(callback){
+  return runSequence(
+    'build',
+    'lint',
+    'bump-version',
+    callback
+  );
+});
