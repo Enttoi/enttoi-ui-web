21d1ab2cc6cc2532fc501261d6b04c77f3852667
Jenya Y. <jenyay@gmail.com>
2015-11-07 23:21:20 +0200

    signalr packages

--- config.js
+++ config.js
@@ -41,6 +41,7 @@ System.config({
     "bootstrap": "github:twbs/bootstrap@3.3.5",
     "core-js": "npm:core-js@0.9.18",
     "fetch": "github:github/fetch@0.9.0",
+    "ms-signalr-client": "npm:ms-signalr-client@2.2.2",
     "text": "github:systemjs/plugin-text@0.0.2",
     "github:aurelia/animator-css@0.16.0": {
       "aurelia-metadata": "github:aurelia/metadata@0.8.0",
@@ -168,8 +169,11 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:font-awesome@4.4.0": {
-      "css": "github:systemjs/plugin-css@0.1.16"
+    "npm:jquery@2.1.4": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:ms-signalr-client@2.2.2": {
+      "jquery": "npm:jquery@2.1.4"
     }
   }
 });
