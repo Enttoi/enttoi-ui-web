a51610f83b8d6f10a9cfc3afd35dadb10568e36c
Jenya Y. <jenyay@gmail.com>
2015-11-07 23:41:40 +0200

    Dashboard has signalr

--- src/dashboard.js
+++ src/dashboard.js
@@ -1,6 +1,30 @@
 import {CabinModel, RoomModel, FloorAreaModel, FloorModel} from 'models/commonModels';
+import $ from 'jquery';
+import 'ms-signalr-client';
 
 export class Dashboard {
+    activate() {  
+        var connection = $.hubConnection('enttoi-api.azurewebsites.net');
+        var hub = connection.createHubProxy('commonHub');
+        var vm = this;
 
+        hub.on('sensorStatePush', function(state) {
 
+            if(!vm.lastState || vm.lastState < state.timestamp)
+                vm.lastState = state.timestamp;
+
+            console.log(JSON.stringify(state)); 
+        });
+
+        /*return*/ connection.start({ })
+            .done(function(){ 
+                console.log('Connected'); 
+                hub.requestInitialState();
+                vm.hubConnected = true;
+            })
+            .fail(function(){ 
+                console.log('Could not connect'); 
+                vm.hubConnected = false;
+            });
+    }
 }
