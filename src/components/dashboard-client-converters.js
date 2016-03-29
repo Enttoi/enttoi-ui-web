44ab9d3ca3d32224af307f7fd76657f1ce3177fe
Jenya Y <jenyay@gmail.com>
2016-03-29 17:34:13 +0300

    Show not exact time of sensor's state; closes #19

--- src/components/dashboard-client-converters.js
+++ src/components/dashboard-client-converters.js
@@ -29,21 +29,25 @@ export class StateCssValueConverter {
   toView(value) {
     switch (value) {
       case models.SENSOR_STATE_FREE: return 'text-success';
-      case models.SENSOR_STATE_OCCUPIED: return 'text-danger'; 
+      case models.SENSOR_STATE_OCCUPIED: return 'text-danger';
       default: return '';
     }
   }
 }
 
 export class FromNowValueConverter {
-  toView(value) {
-    if(!value) throw Error('value of date cannot be null')
+  toView(sensor) {
+    
     var now = moment();
-    var then = moment(value);
+    var then = moment(sensor.stateTimestamp);
     var seconds = now.diff(then, 'seconds');
-    if(seconds < 60 )
+    if (seconds < 60)
       return `${seconds} seconds`;
-    else      
-      return then.fromNow(true);
+    else {
+      if (sensor.state === models.SENSOR_STATE_OCCUPIED)
+        return 'more than 1 minute';
+      else
+        return then.fromNow(true);
+    }
   }
 }
